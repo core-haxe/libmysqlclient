b565ce6f50b452179425dc9617da101ada0cb2dc
Ian Harrigan <ianharrigan@hotmail.com>
2024-09-02 14:05:43 +0200

    externalize window build config

--- src/mysql/RawMySqlClient.hx
+++ src/mysql/RawMySqlClient.hx
@@ -5,22 +5,7 @@ import cpp.RawPointer;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-@:buildXml('
-    <set name="lib_folder" value="${haxelib:libmysqlclient}/lib" />
-    <set name="include_folder" value="${haxelib:libmysqlclient}/lib" />
-    <echo value="Using libmysqlclient from: ${lib_folder}" />
-    <section if="windows">
-        <files id="haxe">
-            <compilerflag value="-I${include_folder}" />
-            <compilerflag value="-I${include_folder}/include" />
-        </files>
-
-        <target id="haxe" tool="linker" toolid="exe">
-            <lib name="${lib_folder}/mysqlclient.lib"/>
-            <lib name="advapi32.lib" />
-        </target>
-    </section>
-')
+@:buildXml('<include name="${haxelib:libmysqlclient}/Build.windows.xml"/>')
 @:include("mysql.h")
 @:unreflective
 extern class RawMySqlClient {
